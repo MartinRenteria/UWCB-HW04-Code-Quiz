1bc20fcc8a9e601faf6cc502f731e161b0b99286
Martin Renteria <renteriamp96@gmail.com>
2020-10-28 11:27:23 -0700

    Added correct answer function that will give points to the user and also a incorrect answer function that will substract time from the user if they get it wrong. Also, changed variable names around in certain areas to make it easier for developer to understand.

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
+// Quiz questions 
+var quizQuestions = [{
+    question: "How do we write an IF statement for executing some code if 'i' is NOT equal to 10?",
+    choices: ["if (i != 10)", "if i <> 10", "if i =! 10", "if (i <= 10)"],
+    answer: "if (i != 10)"
+},
+
+]
+
+
 // Variables for the timer, score, questions, and time remaining
 var timer;
 var timeRemaining = 0;
 var score = 0;
-var questionon = 0;
+var questionon = -1;
 
 // Once the user clicks the start button, the quiz will begin!
 function start() {
@@ -23,10 +33,28 @@ function start() {
     next();
 }
 
-var quizQuestions = [{
-    prompt: "How do we write an IF statement for executing some code if 'i' is NOT equal to 10?",
-    choices: ["if (i != 10)", "if i <> 10", "if i =! 10", "if (i <= 10)"],
-    correctAnswer: "if (i != 10)"
-},
+// If the answer is correct, no changes will be made to the timer and the user will receive 10 points 
+function correctAnswer() {
+    score += 20;
+    next();
+}
 
-]
+// If the answer gets the answer wrong, the user will lose 10 seconds on the timer and not receive points. 
+function incorrectAnswer() {
+    timeRemaining -= 10;
+    next();
+}
+
+// Potential question loop
+function next() {
+    questionon++;
+
+    if (questionon > quizQuestions.length - 1) {
+        GameOver();
+        return;
+
+    var quizlayout = "<h2>" + quizQuestions[questionon].question + "</h2>"
+    }
+
+    document.getElementById("welcome").innerHTML = quizlayout;
+}
